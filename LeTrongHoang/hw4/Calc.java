674bd4914b9b38610af9f66be476b418a998759c
lthoangg <hoanglt.bi9241@st.usth.edu.vn>
2020-07-17 09:50:38 +0700

    Update exercises chapter 4

---
+++ LeTrongHoang/hw4/Calc.java
@@ -0,0 +1,13 @@
+public class Calc
+{
+    static public int add (int a, int b)
+    {
+        return a + b;
+    }
+    static public int minus(int a, int b) {
+        return a - b;
+    }
+    static public int multiply(int a, int b) {
+        return a * b;
+    }
+} 
