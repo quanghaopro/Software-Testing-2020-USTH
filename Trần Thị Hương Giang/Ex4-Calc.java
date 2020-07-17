6c9a816d8121781ab5b4cb1f4fc3e1f52830dcfe
giangtran230300 <59214587+giangtran230300@users.noreply.github.com>
2020-07-17 10:48:01 +0700

    Create Ex4-Calc.java

---
+++ Trần Thị Hương Giang/Ex4-Calc.java
@@ -0,0 +1,25 @@
+
+package test;
+
+public class Calc {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static int divide(int a, int b) {
+       if (b == 0) {
+           throw new ArithmeticException("Cannot divided by zero!");
+       }
+       else {
+           return a / b;
+       }
+    }
+}
