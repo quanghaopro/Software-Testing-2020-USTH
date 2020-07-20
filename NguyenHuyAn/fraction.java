80c4f1ed33bd73202df742308e3036b8788bba28
HuyAnNg <59855994+HuyAnNg@users.noreply.github.com>
2020-07-21 01:10:14 +0700

    Add files via upload

---
+++ NguyenHuyAn/fraction.java
@@ -0,0 +1,27 @@
+package Demo;
+
+public class fraction {
+	private int T;
+	private int M;
+
+	public fraction() {
+		setT(1);
+		setM(2);
+	}
+
+	public int getT() {
+		return T;
+	}
+
+	public void setT(int t) {
+		T = t;
+	}
+
+	public int getM() {
+		return M;
+	}
+
+	public void setM(int m) {
+		M = m;
+	}
+}
