1f47338d2c920339dc427e322c40665cb81159bc
NLTD1209 <45072379+NLTD1209@users.noreply.github.com>
2020-07-21 00:06:39 +0700

    Fail tests

---
+++ NguyenLeTuanDuy/calc.java
@@ -0,0 +1,12 @@
+// Introduction to Software Testing
+// Authors: Paul Ammann & Jeff Offutt
+// Chapter 3; page ??
+// See CalcTest.java, DataDrivenCalcTest.java for JUnit tests
+
+public class calc
+{
+    static public int add (int a, int b)
+    {
+        return a + b;
+    }
+}
